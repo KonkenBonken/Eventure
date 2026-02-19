7c017f7e431d4b3846070dcda69390c8fadf9b17
auumkat <aube6172@student.uu.se>
2026-02-19 18:07:42 +0100

    Implemented functions get_ticket and make_ticket

--- backend/lib/ticket.ts
+++ backend/lib/ticket.ts
@@ -1,17 +1,27 @@
-import {ph_empty} from './pkd/hashtables.js';
+import {ph_empty, ph_insert, ph_lookup} from './pkd/hashtables.js';
 import {type EventId} from './event.js';
-import {hash_function, type TicketId} from "./generate_ids.js";
+import {hash_function, generate_new_ticket_id, type TicketId} from "./generate_ids.js";
 
 export interface Ticket {
     event_id: EventId,
     ticket_id: TicketId
 }
 
-const tickets = ph_empty<TicketId, Ticket>(100, hash_function);
+export const tickets = ph_empty<TicketId, Ticket>(100, hash_function);
 
 export function get_ticket(ticket_id: TicketId): Ticket | null {
+    const find_ticket = ph_lookup<TicketId, Ticket>(tickets, ticket_id);
+    if(find_ticket === undefined) {
+        return null;
+    }
+    return find_ticket;
 }
 
 export function make_ticket(event_id: EventId): Ticket {
 // Generate id, create ticket, store ticket in Tickets and return Ticket
+    const ticket_id = generate_new_ticket_id(); // generate_new_ticket_id is not finished !!!
+    const current_ticket = {event_id, ticket_id};
+    // adding ticket to tickets:
+    ph_insert<TicketId, Ticket>(tickets, ticket_id, current_ticket);
+    return current_ticket;
 }
