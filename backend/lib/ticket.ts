ba92b8b12c4035517c6a712ee66895871c080ef4
auumkat <aube6172@student.uu.se>
2026-02-19 18:10:16 +0100

    Forgott to delete a note in the code...

--- backend/lib/ticket.ts
+++ backend/lib/ticket.ts
@@ -19,7 +19,7 @@ export function get_ticket(ticket_id: TicketId): Ticket | null {
 
 export function make_ticket(event_id: EventId): Ticket {
 // Generate id, create ticket, store ticket in Tickets and return Ticket
-    const ticket_id = generate_new_ticket_id(); // generate_new_ticket_id is not finished !!!
+    const ticket_id = generate_new_ticket_id();
     const current_ticket = {event_id, ticket_id};
     // adding ticket to tickets:
     ph_insert<TicketId, Ticket>(tickets, ticket_id, current_ticket);
