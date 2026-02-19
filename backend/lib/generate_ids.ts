7838ae807567a6e36f213de084914840133a0db7
auumkat <aube6172@student.uu.se>
2026-02-19 17:59:02 +0100

    Function that generates id's will now check for duplicates in tickets hash table. This ensures that all ticket id's will be unique

--- backend/lib/generate_ids.ts
+++ backend/lib/generate_ids.ts
@@ -1,12 +1,20 @@
-import {type TicketId} from './ticket.js';
+import { ph_lookup } from "./pkd/hashtables.js";
+import { tickets, Ticket } from "./ticket.js";
 
 /** A ticket id consists of a six character long string consisting of characters a-z and 0-9 */
 export type TicketId = string;
 
 export function generate_new_ticket_id(): TicketId {
     // We generate a random number between 0 and 36⁶-1 and then return it as a six character long string in base 36
-    return Math.floor(Math.random() * 36 ** 6).toString(36).padStart(6, '0');
-    // TODO: Also check for duplicates
+    const id = Math.floor(Math.random() * 36 ** 6).toString(36).padStart(6, '0');
+    
+    // Check for duplicates
+    const is_unique = ph_lookup<TicketId, Ticket>(tickets, id);
+    if (is_unique === undefined) {
+        return id;
+    }
+    // else generate a new id
+    return generate_new_ticket_id();
 }
 
 export function hash_function(k: TicketId): number {
