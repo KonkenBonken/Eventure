79f7026494d538bdf613fe56685199babf3a5f61
KonkenBonken <konrad05pettersson@gmail.com>
2026-02-20 13:46:43 +0100

    Added fetching and creating of HTML elements for each event on the front-end

--- frontend/index.ts
+++ frontend/index.ts
@@ -1,1 +1,21 @@
-console.log('This should show up in the console on the main page');
+import type {Event} from "../backend/lib/event.js";
+
+function create_event_card(event: Event): HTMLElement {
+    const card = document.createElement("div");
+    card.classList.add("event");
+    card.innerHTML = `
+        <h2>${event.title}</h2>
+        <p>${event.description}</p>
+        <p>${event.price}:-</p>
+        <hr>
+    `;
+    return card;
+}
+
+const event_list_response = await fetch('/api/events');
+const event_list: Array<Event> = await event_list_response.json();
+console.log(event_list);
+
+for (const event of event_list) {
+    document.body.append(create_event_card(event));
+}
