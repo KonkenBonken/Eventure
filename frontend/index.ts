b16a090c6f9113c716df327db293fda56fe91038
KonkenBonken <konrad05pettersson@gmail.com>
2026-02-21 13:38:26 +0100

    Added button for booking tickets and then displaying the booked ticket id

--- frontend/index.ts
+++ frontend/index.ts
@@ -1,15 +1,25 @@
 import type {Event} from "../backend/lib/event.js";
-import {UserId} from "../backend/lib/generate_ids.js";
+import {TicketId, UserId} from "../backend/lib/generate_ids.js";
 
 function create_event_card(event: Event): HTMLElement {
     const card = document.createElement("div");
     card.classList.add("event");
     card.innerHTML = `
+        <hr>
         <h2>${event.title}</h2>
         <p>${event.description}</p>
         <p>${event.price}:-</p>
-        <hr>
+        <button>Book</button>
     `;
+
+    card.querySelector('button')?.addEventListener('click', async () => {
+        const ticket_response = await fetch(`/api/book/${event.event_id}/${user_id}`);
+        const ticket_id: TicketId = await ticket_response.text();
+        const ticket_element = document.createElement("p");
+        ticket_element.innerHTML = `Your ticket id: <code>${ticket_id}</code>`;
+        card.append(ticket_element);
+    });
+
     return card;
 }
 
@@ -17,12 +27,10 @@ const event_list_response = await fetch('/api/events');
 const event_list: Array<Event> = await event_list_response.json();
 console.log(event_list);
 
-for (const event of event_list) {
-    document.body.append(create_event_card(event));
-}
-
 // User gets a unique code to use when booking a ticket
 const user_id_response = await fetch('/api/get_new_user_id');
 const user_id: UserId = await user_id_response.text();
 
-// Show the user their ticket
+for (const event of event_list) {
+    document.body.append(create_event_card(event));
+}
