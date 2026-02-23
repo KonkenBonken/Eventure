3003c95bb5edf4e7aa49c897df39dd6c94cd7b77
KonkenBonken <konrad05pettersson@gmail.com>
2026-02-23 16:14:01 +0100

    Added function make_event for creating and storing events

--- backend/lib/event.ts
+++ backend/lib/event.ts
@@ -1,15 +1,15 @@
 import { ph_insert } from './pkd/hashtables.js';
-import {type EventId, lookup_id, make_ht} from './generate_ids.js';
+import {type EventId, generate_new_id, lookup_id, make_ht} from './generate_ids.js';
 
 // A string representing a ISO timestamp
 type Timestamp = string;
 
 export interface Event {
-    event_id: EventId;
-    title: string;
-    description: string;
-    timestamp: Timestamp;
-    price: number;
+    event_id: EventId,
+    title: string,
+    description: string,
+    timestamp: Timestamp,
+    price: number
 }
 
 
@@ -42,30 +42,37 @@ export function get_date(Event: Event): Date {
     return new Date(Event.timestamp);
 }
 
-//hardcoded examples
-const event_ex1: Event = {
-    event_id: "123456",
+/**
+ * Creates an event record and stores it in the events table
+ * @returns The created event record
+ */
+export function make_event(data: Omit<Event, 'event_id'>): Event {
+    const event_id = generate_new_id(events);
+    const new_event = {...data, event_id};
+    // adding event to events:
+    ph_insert(events, event_id, new_event);
+    return new_event;
+}
+
+// add hardcoded examples to the events table
+make_event({
     title: "Hot chocochug",
     description: "Drink a liter of hot chocolate challenge, hot chocolate provided",
     timestamp: new Date(2026, 1, 25).toJSON(),
     price: 40,
-}
-const event_ex2: Event = {
-    event_id: "789abc",
+});
+
+make_event({
     title: "Bee hive building",
     description: "Come build some beehives for spring!",
     timestamp: new Date(2026, 3, 20).toJSON(),
     price: 10,
-}
-const event_ex3: Event = {
-    event_id: "defghi",
+});
+
+make_event({
     title: "Beverage ball",
     description: "Dance around in outfits inspired by your favorite drink!",
     timestamp: new Date(2026, 2, 3).toJSON(),
     price: 150,
-}
-//add examples to events hashtable
-ph_insert(events, event_ex1.event_id, event_ex1);
-ph_insert(events, event_ex2.event_id, event_ex2);
-ph_insert(events, event_ex3.event_id, event_ex3);
+});
 
