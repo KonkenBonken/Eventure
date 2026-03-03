83f9e97649f0c8942d6a835c3839cd50d17cf759
Leona Bergström <leona.bergstrom.0055@student.uu.se>
2026-03-03 16:01:34 +0100

    make_ticket now also checks if the user already has a ticket for that event

--- backend/lib/ticket.ts
+++ backend/lib/ticket.ts
@@ -23,16 +23,39 @@ export function get_ticket(ticket_id: TicketId): Ticket | null {
 }
 
 /**
+ * Checks if user already has a ticket for event
+ * @param event_id The id of the event
+ * @param user_id The id of the user
+ * @returns true if the user has a ticket for the event and false if they dont.
+ */
+export function Already_has_ticket_check(event_id: EventId, username: string): true | false {
+    
+    //checks for the eventId in all the users tickets 
+    function check_for_ticket(event_id: EventId, username: string): boolean {
+        const users_tickets = get_tickets_for_user(username);
+
+        return users_tickets.some(ticket => ticket.event_id === event_id);
+    }
+    
+    if (check_for_ticket(event_id, username)) {
+        return true
+    } else {
+        return false
+    }
+}
+
+/**
  * Creates a ticket record and stores it in the tickets table
  * @param event_id The id of the event
  * @param username The username of the user
  * @returns The created ticket record if successfully booked,
- *          if event is not found or is sold out, returns false
+ *          if user already has a ticket for the event,
+ *          event is not found or is sold out, returns false
  */
 export function make_ticket(event_id: EventId, username: string): Ticket | false {
     const event = get_event(event_id);
     // If event is not found or is sold out, return false
-    if (event === null || is_sold_out(event)) {
+    if (event === null || is_sold_out(event) || Already_has_ticket_check(event_id, username)) {
         return false;
     }
 
@@ -43,6 +66,7 @@ export function make_ticket(event_id: EventId, username: string): Ticket | false
     ph_insert(tickets, ticket_id, new_ticket);
     return new_ticket;
 }
+
 
 /**
  * Gets all tickets for a user
